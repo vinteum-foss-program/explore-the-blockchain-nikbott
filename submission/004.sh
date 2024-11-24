90720fcc58d6e3b0d2d4b2c7c0e412cb21dca41b
nikbott <viniciusreyeskury@protonmail.com>
2024-11-24 17:25:08 -0300

    004 - Solved

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,9 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+
+# format as taproot descriptor for 100th address
+desc="tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/100)"
+
+# get checksum
+desc=$(bitcoin-cli getdescriptorinfo "$desc" | jq -r '.descriptor')
+bitcoin-cli deriveaddresses "$desc" | jq -r '.[]'
