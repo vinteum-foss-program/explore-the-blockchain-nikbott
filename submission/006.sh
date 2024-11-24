cc4e406c269da4e183e191d253edfcc3042d99c9
nikbott <viniciusreyeskury@protonmail.com>
2024-11-24 17:25:25 -0300

    006 - Solved

--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,15 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
+
+block1_height=256128
+block2_height=257343
+
+block1_hash=$(bitcoin-cli getblockhash "$block1_height")
+block2_hash=$(bitcoin-cli getblockhash "$block2_height")
+
+block1=$(bitcoin-cli getblock "$block1_hash")
+block2=$(bitcoin-cli getblock "$block2_hash" 2)
+
+coinbase_txid=$(echo "$block1" | jq -r '.tx[0]')
+
+# no need for details from the coinbase tx since it's a single output
+echo "$block2" | jq -r --arg txid "$coinbase_txid" '.tx[] | select(.vin[]?.txid == $txid) | .txid'
