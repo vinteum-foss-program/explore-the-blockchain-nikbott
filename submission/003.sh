17891d89517d3486375e94070bfb0d7951534ea1
nikbott <viniciusreyeskury@protonmail.com>
2024-11-24 12:43:49 -0300

    003 - Solved

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,6 @@
 # How many new outputs were created by block 123,456?
+
+block_height=123456
+block_hash=$(bitcoin-cli getblockhash "$block_height")
+
+bitcoin-cli getblock "$block_hash" 2 | jq -r '[.tx[].vout | length] | add'
