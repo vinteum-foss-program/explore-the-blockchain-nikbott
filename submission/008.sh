feda8ed2c947e3fbdb9c8e8e634464d6ff962092
nikbott <viniciusreyeskury@protonmail.com>
2024-11-24 17:27:01 -0300

    008 - Solved

--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,6 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+
+txid="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
+
+bitcoin-cli getrawtransaction "$txid" 1 | jq -r '.vin[0].txinwitness[2] | .[4:70]'
